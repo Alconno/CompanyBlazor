e639880a31240a4d4152157424c7541a51992b9a
Alcon0x45 <74137409+Alcon0x45@users.noreply.github.com>
2023-05-02 06:29:04 +0200

    Sorted pages over specific folders

--- CompanyBlazor5/Client/Pages/Employees/EmployeeDepartmentBase.cs
+++ CompanyBlazor5/Client/Pages/Employees/EmployeeDepartmentBase.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace CompanyBlazor5.Client.Pages
+namespace CompanyBlazor5.Client.Pages.Employees
 {
     public class EmployeeDepartmentBase
     {
